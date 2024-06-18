1b1b5decd3d951f8683b2bab5e35056ae9393fea
Larissa Djunbawski <larydjm@gmail.com>
2024-06-18 08:41:43 -0300

    feat: adicionado ordem ao array

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 let numeros = [4,3,1,5,5,7,,2,-47,2001]
+
+for(let i = 0; i < numeros.length; i++){
+    console.log("no primeiro laço", + numeros[i])
+    for (let j = i + 1; j < numeros.length; j++){
+        console.log("no segundo laço "+ numeros[j])
+
+        if (numeros[i]>numeros[j]){
+
+         let auxiliar = numeros[i]
+         numeros[i] = numeros[j]
+         numeros[j] = auxiliar
+
+
+        }
+    }
+}
+console.log(numeros)
