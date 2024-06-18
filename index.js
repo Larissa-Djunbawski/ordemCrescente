a0678f66e690f5b47273763432d139752cf9f322
Larissa Djunbawski <larydjm@gmail.com>
2024-06-18 08:47:24 -0300

    feat:  remoção dos valores iguais

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
-let numeros = [4,3,1,5,5,7,,2,-47,2001]
+let numeros = [4, 3, 1, 5, 5, 7, 7, 2, -47, 2001];
 
-for(let i = 0; i < numeros.length; i++){
-    console.log("no primeiro laço", + numeros[i])
-    for (let j = i + 1; j < numeros.length; j++){
-        console.log("no segundo laço "+ numeros[j])
-
-        if (numeros[i]>numeros[j]){
-
-         let auxiliar = numeros[i]
-         numeros[i] = numeros[j]
-         numeros[j] = auxiliar
-
-
-        }
+for (let i = 0; i < numeros.length; i++) {
+  for (let j = i + 1; j < numeros.length; j++) {
+    if (numeros[i] > numeros[j]) {
+      let auxiliar = numeros[i];
+      numeros[i] = numeros[j];
+      numeros[j] = auxiliar;
     }
+  }
 }
-console.log(numeros)
+console.log(numeros);
+
+for (let i = 0; i < numeros.length; i++) {
+  for (let j = i + 1; j < numeros.length; j++) {
+    if (numeros[i] == numeros[j]) {
+      numeros.splice(j, 1);
+      j--;
+    }
+  }
+}
+console.log(numeros);
